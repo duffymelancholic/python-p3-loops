6a9c537f3e9e5719d10e8910ab6126103dfecf50
Nicholas obonyo abala <nicholasobonyo444@gmail.com>
2025-08-09 13:32:10 +0300

    Lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i-= 1
+        print("Happy New Year!")
+    
 
 def square_integers(int_list):
+    return [x**2 for x in int_list ]
+    
+
+    
     # code goes here!
-    pass
+    
 
 def fizzbuzz():
+    for i in range(1, 101):
     # code goes here!
-    pass
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
